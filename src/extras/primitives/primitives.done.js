0081115fea09214b13c112bebd883cd8c78e03c6
andiwinata <andi.winata@hotmail.com>
2018-03-06 21:12:54 +1100

    Rename to example, and moving to extras

---
+++ src/extras/primitives/primitives.done.js
@@ -0,0 +1,53 @@
+// @flow
+
+let a: boolean
+a = true
+a = false
+a = 0 // error
+
+
+let b: number
+b = 42
+b = 3.14
+b = NaN
+b = Infinity
+b = 'foo' // error
+
+
+let c: string
+c = 'foo'
+c = 'foo' + 42
+c = 'foo' + {} // error
+
+
+function onlyBoolean(value: boolean) {
+}
+
+let res
+res = onlyBoolean(true)
+res = onlyBoolean('foo') // error
+res = onlyBoolean(null) // error
+
+
+function onlyTakingNull(value: null) {
+}
+
+onlyTakingNull(null)
+onlyTakingNull(undefined) // error
+onlyTakingNull(5) // error
+
+
+function onlyUndefined(value: void) {
+}
+
+onlyUndefined(undefined)
+onlyUndefined(null) // error
+onlyUndefined(5) // error
+
+
+function maybeType(value: ?number) {
+}
+
+maybeType(5)
+maybeType(null)
+maybeType(undefined)
---
+++ src/extras/primitives/primitives.js
@@ -0,0 +1,16 @@
+// @flow
+
+function onlyBoolean(value: boolean) {
+}
+
+
+function onlyTakingNull(value: null) {
+}
+
+
+function onlyUndefined(value: void) {
+}
+
+
+function maybeType(value: ?number) {
+}
